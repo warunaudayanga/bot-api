0a05649712f8a5db101abf589286f325450a88e3
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2024-03-28 17:09:40 +0530

    Added Booking module

--- src/travel-assist/controllers/booking.controller.ts
+++ src/travel-assist/controllers/booking.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from "@nestjs/common";
 import { BookingService } from "../services/booking.service";
 import { CreateBookingDto } from "../dtos/create-booking.dto";
 import { UpdateBookingDto } from "../dtos/update-booking.dto";
@@ -8,21 +8,25 @@ export class BookingController {
     constructor(private readonly bookingService: BookingService) {}
 
     @Get(":botId")
+    @HttpCode(HttpStatus.OK)
     async getBookings(@Param("botId") botId: string) {
         return await this.bookingService.getMany({ where: { botId } });
     }
 
     @Get(":botId/:id")
+    @HttpCode(HttpStatus.OK)
     async getBookingById(@Param("botId") botId: string, @Param("id") id: string) {
         return await this.bookingService.getOne({ where: { botId, id } });
     }
 
     @Post(":botId")
+    @HttpCode(HttpStatus.OK)
     async createBooking(@Param("botId") botId: string, @Body() createBookingDto: CreateBookingDto) {
         return await this.bookingService.save({ ...createBookingDto, botId });
     }
 
     @Patch(":botId/:id")
+    @HttpCode(HttpStatus.OK)
     async updateBooking(
         @Param("botId") botId: string,
         @Param("id") id: string,
@@ -32,6 +36,7 @@ export class BookingController {
     }
 
     @Delete(":botId/:id")
+    @HttpCode(HttpStatus.OK)
     async deleteBooking(@Param("botId") botId: string, @Param("id") id: string) {
         return await this.bookingService.deleteBooking(botId, id);
     }
