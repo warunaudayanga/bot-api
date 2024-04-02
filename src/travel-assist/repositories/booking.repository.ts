95ce3bfe4a139a765c1ab10c26bd2a6d0971a84a
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2024-04-02 14:27:28 +0530

    Update APIs

--- src/travel-assist/repositories/booking.repository.ts
+++ src/travel-assist/repositories/booking.repository.ts
@@ -3,10 +3,18 @@ import { Injectable } from "@nestjs/common/decorators/core/injectable.decorator"
 import { InjectRepository } from "@nestjs/typeorm/dist/common/typeorm.decorators";
 import { Repository } from "typeorm/repository/Repository";
 import { BookingEntity } from "../entities/booking.entity";
+import { DeepPartial, EntityManager, SaveOptions } from 'typeorm';
 
 @Injectable()
 export class BookingRepository extends BaseRepository<BookingEntity> {
     constructor(@InjectRepository(BookingEntity) protected readonly bookingRepository: Repository<BookingEntity>) {
         super(bookingRepository);
+    }
+
+    async save<T extends DeepPartial<BookingEntity>>(entity: T, options?: SaveOptions, manager?: EntityManager): Promise<T & BookingEntity> {
+        return this.transaction(async (entityManager) => {
+            await entityManager.delete(BookingEntity, { botId: entity.botId, phone: entity.phone });
+            return entityManager.save(BookingEntity, entity, options);
+        });
     }
 }
